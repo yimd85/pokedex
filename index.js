de830f46fa052625ae5752e623383dbc84c86a45
yimd85 <yimd85@gmail.com>
2023-05-19 21:20:49 -0400

    error handling

--- index.js
+++ index.js
@@ -2,34 +2,40 @@ const $form = $("form");
 
 $form.on("submit", (event) => {
     event.preventDefault();
-    const formData = new FormData(event.target);    
+    const formData = new FormData(event.target);
     const pokemon = formData.get("pokemon");
-    
-    $('[name="pokemon"]')[0].value = '';
+
+    $('[name="pokemon"]')[0].value = "";
 
     const pokeapi = "https://pokeapi.co/api/v2/pokemon/";
-    const $result = $('.ds-middle_result');
+    
+    const $screen = $(".ds-middle_screen");
+    $screen.empty();
+    
+    const $result = $(".ds-middle_result");
     $result.empty();
     $result.html(`<div>Loading...</div>`);
-    const $middle = $('.ds-middle_screen');
-    $middle.empty();
 
     fetch(`${pokeapi}${pokemon.toLowerCase()}`)
-        .then(response => {
+        .then((response) => {
             return response.json();
         })
-        .then(data => {
+        .then((data) => {
+            $screen.html(
+                `<img src=${data.sprites.front_shiny || data.sprites.front_default} alt=${data.name}>`
+            );
             $result.empty();
             $result.html(`
                 <div><b>name:&nbsp;</b>${data.name}</div>
                 <div><b>id:&nbsp;</b>${data.id}</div>
                 <div><b>weight:&nbsp;</b>${data.weight}</div>
-                <div><b>types:&nbsp;</b>${data.types.map(v => v.type.name).toString()}</div>
+                <div><b>types:&nbsp;</b>${data.types.map((v) => v.type.name).toString()}</div>
             `);
-            $middle.empty();
-            $middle.html(
-                `<img src=${data.sprites.front_shiny || data.sprites.front_default} alt=${data.name}>`
-            )
         })
-        .catch(() => alert('invalid pokemon'));
-})
+        .catch(() => {
+            $screen.html(
+                `<div class="ds-middle_screen_not-found"> 404 Pokemon not found</div>`
+            );
+            $result.empty();
+        });
+});
